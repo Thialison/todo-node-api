77f6ce324c122abcfbf5002318c1625f3463997f
Thialison Souza <thialison.souza@hotmail.com>
2020-08-28 16:31:09 -0300

    Add http verbs

--- src/handlers/todo.js
+++ src/handlers/todo.js
@@ -7,11 +7,28 @@ const response = todo => ({
     title: todo.title,
     description: todo.description,
     status: todo.status,
+    end_date: todo.end_date,
   },
   links: {
     self: `/api/v1/todos/${todo.id}`,
   }
 });
+
+const notFound = (req) => ({
+  type: 'todos',
+  id: req.id,
+  status: 'NOT FOUND',
+  code: '404'
+})
+
+const getOne = async (req, h) => {
+  try {
+    const todo = await TodoModel.findById(req.params.id);
+    return h.response({ data: response(todo) }).code(200);
+  } catch (error) {
+    return h.response({ erros: notFound(req.params) }).code(404);
+  }
+}
 
 const getAll = async (req, h) => {
   const todos = await TodoModel.find({});
@@ -24,19 +41,25 @@ const save = async (req, h) => {
 
   todo.title = title;
   todo.description = description;
-  todo.status = status;
-
-  if (end_date !== undefined) {
-    todo.end_date = end_date;
-  }
-
+  status !== undefined ? todo.status = status : null;
+  end_date !== undefined ? todo.end_date = end_date : null;
   await todo.save();
-
   return h.response({ data: response(todo) }).code(201)
+}
 
+const remove = async (req, h) => {
+  const params = req.params;
+  try {
+    await TodoModel.findOneAndDelete({ _id: params.id });
+    return h.response().code(204);
+  } catch (error) {
+    return h.response({ errors: notFound(params) }).code(404);
+  }
 }
 
 module.exports = {
   getAll,
   save,
+  getOne,
+  remove
 }
